ede3e4329820d7db645bbae6e7db66d9b73017ba
francisco oliva <francisco.j.oliva@gmail.com>
2022-04-15 02:29:30 -0400

    Challenge completed.

--- models/queries.js
+++ models/queries.js
@@ -9,7 +9,12 @@ const insertUser = async (email, nombre, password, experiencia, especialidad, fo
     const query = 'INSERT INTO skaters (email, nombre, password, anos_experiencia, especialidad, foto, estado, is_admin) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *';
 
     try {
-        const client = new Client();
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         await client.connect();
         const counter = await client.query('SELECT COUNT(*) AS count FROM skaters');
         const [{ count }] = counter.rows;
@@ -28,7 +33,12 @@ const insertUser = async (email, nombre, password, experiencia, especialidad, fo
 
 const getUsers = async() => {
     try {
-        const client = new Client();
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         await client.connect();
         const result = await client.query(`SELECT * FROM skaters ORDER BY id`);
         return result.rows;
@@ -41,7 +51,12 @@ const getUser = async(email, password) => {
     const query = `SELECT * FROM skaters WHERE email = $1 AND password= $2 LIMIT 1`;
     const values = [email, password];
     try {
-        const client = new Client();
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         await client.connect();
         const result = await client.query(query, values);
         return result.rows[0];
@@ -54,7 +69,12 @@ const getUserById = async(id) => {
     const query = `SELECT * FROM skaters WHERE id = $1`;
     const values = [id];
     try {
-        const client = new Client();
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         await client.connect();
         const result = await client.query(query, values);
         return result.rows[0];
@@ -68,7 +88,12 @@ const updateUser = async (id, user) => {
     const query = 'UPDATE skaters SET nombre=$2, password=$3, anos_experiencia=$4, especialidad=$5 WHERE id=$1 RETURNING*';
     const values = [id, user.nombre, user.password, user.experiencia, user.especialidad];
     try {
-        const client = new Client();
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         await client.connect();
         const result = await client.query(query, values);
         return result.rows[0];
@@ -82,7 +107,12 @@ const updateStatus = async (id, status) => {
     const query = 'UPDATE skaters SET estado=$2 WHERE id=$1 RETURNING*';
     const values = [id, status];
     try {
-        const client = new Client();
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
         await client.connect();
         const result = await client.query(query, values);
         return result.rows[0];
